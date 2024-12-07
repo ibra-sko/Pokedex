517ba5a908d59537d791012785936267058f6de7
ibra-sko <ibrahim.sakotraore@gmail.com>
2024-12-07 05:02:04 +0100

    optimisation

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { PokemonProvider } from "./Context/PokemonContext";
 import { LanguageProvider } from "./Context/LanguageContext";
 import Search from "./Components/Search/Search.jsx";
-import PokemonList from "./Components/PokemonList/PokemonList.jsx"; // Correct import path
+import PokemonList from "./Components/PokemonList/PokemonList.jsx";
 import Header from "./Components/Header/Header.jsx";
-import PokemonDetails from "./Components/PokemonDetails/Pokemondetail.jsx"; // Correct import path
+import PokemonDetails from "./Components/PokemonDetails/Pokemondetail.jsx";
 
 function App() {
     return (
@@ -28,9 +28,6 @@ function App() {
 
                             {/* Route pour les détails d'un Pokémon */}
                             <Route path="/pokemon/:pokemonId" element={<PokemonDetails />} />
-
-                            {/* Route pour gérer les URL inconnues */}
-                            <Route path="*" element={<Navigate to="/" replace />} />
                         </Routes>
                     </div>
                 </Router>
